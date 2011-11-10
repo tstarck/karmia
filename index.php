df370318a949fc75e70a49d3462e239499e78064
Tuomas Starck <dev@starck.fi>
2011-11-10 15:02:25 +0200

    Testi tietokannalle

--- index.php
+++ index.php
@@ -1,14 +1,32 @@
-<html>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>"Örrr"</title>
+<title>Karmia</title>
 </head>
 <body>
-
 <?php
 
-echo "Moi";
+require 'pgkanto.php';
+
+function tulosta($viesti) {
+	echo "<p>$viesti</p>\n";
+}
+
+$kanto = new PGDB();
+
+if (!$kanto->ok()) die;
+
+$kysely = "SELECT * FROM laji WHERE id = 0";
+
+if ($kanto->query($kysely)) {
+	echo "<pre>\n";
+	var_dump($kanto->getline());
+	echo "</pre>\n";
+}
+else {
+	tulosta(":-(");
+}
 
 ?>
-
 </body>
 </html>
