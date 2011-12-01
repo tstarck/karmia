2907da1d223dd1efbff59f61ed2655c43a8f37f1
Tuomas Starck <dev@starck.fi>
2011-12-01 20:28:39 +0200

    Testi-indeksi.

--- index.php
+++ index.php
@@ -1,32 +1,12 @@
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>Karmia</title>
-</head>
-<body>
 <?php
 
-require 'pgkanto.php';
+require 'auth.php';
 
-function tulosta($viesti) {
-	echo "<p>$viesti</p>\n";
-}
+$tunnistus = new AUTH();
 
-$kanto = new PGDB();
-
-if (!$kanto->ok()) die;
-
-$kysely = "SELECT * FROM laji WHERE id = 0";
-
-if ($kanto->query($kysely)) {
-	echo "<pre>\n";
-	var_dump($kanto->getline());
-	echo "</pre>\n";
-}
-else {
-	tulosta(":-(");
+if (!$tunnistus->ok()) {
+	header("Location: login.xhtml");
+	exit;
 }
 
 ?>
-</body>
-</html>
