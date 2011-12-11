78241e0267fac586a3201c54e73f3e9b28f965b8
Tuomas Starck <dev@starck.fi>
2011-12-11 02:45:04 +0200

    Tietokantamallin kevennys.

---
+++ data.sql
@@ -0,0 +1,32 @@
+
+ -- Testidataa
+
+INSERT INTO lajit VALUES (NULL, 'paraguaynanakonda', 'Eunectes notaeus', 1, 'keltainen, mustat täplät', 1, 'NE');
+INSERT INTO lajit VALUES (NULL, 'viheranakonda', 'Eunectes murinus', 1, 'oliivinvihreä, mustat täplät', 1, '');
+INSERT INTO lajit VALUES (NULL, 'aavikkotaipaani', 'Oxyuranus microlepitodus', 0, 'talvella ruskea, kesällä oliivinvihreä', 3, '');
+INSERT INTO lajit VALUES (NULL, 'teksasinkalkkarokäärme', 'Crotalus atrox', 3, 'kirjava harmaa ruskea', 3, 'LC');
+INSERT INTO lajit VALUES (NULL, 'kuningaskobra', 'Ophiophagus hannah', 9, 'oliivinvihreä, musta, keltaiset raidat', 3, 'VU');
+INSERT INTO lajit VALUES (NULL, 'mustamamba', 'Dendroaspis polylepsis', 5, 'oliivinvihreä, metallinharmaa', 3, 'LC');
+INSERT INTO lajit VALUES (NULL, 'vihermamba', 'Dendroaspis angusticeps', 5, 'ruohonvihreä, keltaisenvihreä vatsa', 3, '');
+INSERT INTO lajit VALUES (NULL, 'kyy', 'Vipera berus', 5, 'harmaanruskea, sahalaitakuvio', 2, 'LC');
+INSERT INTO lajit VALUES (NULL, 'rantakäärme', 'Natrix natrix', 5, 'oliivinvihreä, harmaanvihreä, niskatäplät', 1, 'LC');
+
+INSERT INTO kaarmeet VALUES (NULL, 'Enska', SELECT id FROM lajit WHERE nimi = 'paraguaynanakonda');
+INSERT INTO kaarmeet VALUES (NULL, 'Ofelia', SELECT id FROM lajit WHERE nimi = 'viheranakonda');
+INSERT INTO kaarmeet VALUES (NULL, 'Tuomas', SELECT id FROM lajit WHERE nimi = 'aavikkotaipaani');
+INSERT INTO kaarmeet VALUES (NULL, 'Alf', SELECT id FROM lajit WHERE nimi = 'teksasinkalkkarokäärme');
+INSERT INTO kaarmeet VALUES (NULL, 'Krtek', SELECT id FROM lajit WHERE nimi = 'kuningaskobra');
+INSERT INTO kaarmeet VALUES (NULL, 'Mustikka', SELECT id FROM lajit WHERE nimi = 'mustamamba');
+INSERT INTO kaarmeet VALUES (NULL, 'Billy-Bob', SELECT id FROM lajit WHERE nimi = 'vihermamba');
+INSERT INTO kaarmeet VALUES (NULL, 'Kalevi', SELECT id FROM lajit WHERE nimi = 'kyy');
+INSERT INTO kaarmeet VALUES (NULL, 'Rane', SELECT id FROM lajit WHERE nimi = 'rantakäärme');
+
+ -- LC, Least Concern, elinvoimainen
+ -- NT, Near Threatened, silmälläpidettävä
+ -- VU, Vulnerable, vaarantunut
+ -- EN, Endangered, erittäin uhanalainen
+ -- CR, Critically Endangered, äärimmäisen uhanalainen
+ -- EW, Extinct in the Wild, luonnosta hävinnyt
+ -- EX, Extinct, hävinnyt
+ -- DD, Data Deficient, puutteellisesti tunnettu
+ -- NE, Not Evaluated, arvioimatta jätetty
--- karmia.sql
+++ karmia.sql
@@ -18,10 +18,9 @@ CREATE TABLE lajit (
 	nimi varchar(40),
 	latin varchar(100),
 	alkupera int,
-	vari int,
-	myrkyllisyys int,
-	aggressiivisuus int,
-	uhanalaisuus varchar(2)
+	vari varchar(100),
+	myrkyllisyys int DEFAULT 0,
+	uhanalaisuus varchar(2) DEFAULT ''
 );
 
 CREATE TABLE lainat (
@@ -37,32 +36,32 @@ CREATE TABLE alkupera (
 	alkupera varchar(20)
 );
 
-CREATE TABLE vari (
-	id int PRIMARY KEY,
-	vari varchar(20)
-);
-
 CREATE TABLE myrkyllisyys (
 	id int PRIMARY KEY,
 	myrkyllisyys varchar(20)
 );
 
-CREATE TABLE aggressiivisuus (
-	id int PRIMARY KEY,
-	aggressiivisuus varchar(20)
-);
+INSERT INTO lajit VALUES (0, 'tuntematon', 'obscura vermis', NULL, '', 0, '');
 
-INSERT INTO lajit VALUES (0, 'ei tiedossa', 'obscura', 0, 0, 0, 0, '?');
-INSERT INTO alkupera VALUES (0, 'ei tiedossa');
-INSERT INTO vari VALUES (0, 'ei tiedossa');
+INSERT INTO alkupera VALUES (0, 'Australia');
+INSERT INTO alkupera VALUES (1, 'Etelä-Amerikka');
+INSERT INTO alkupera VALUES (2, 'Väli-Amerikka');
+INSERT INTO alkupera VALUES (3, 'Pohjois-Amerikka');
+INSERT INTO alkupera VALUES (4, 'Eurooppa');
+INSERT INTO alkupera VALUES (5, 'Afrikka');
+INSERT INTO alkupera VALUES (6, 'Kaukasia');
+INSERT INTO alkupera VALUES (7, 'Aasia');
+INSERT INTO alkupera VALUES (8, 'Etelä-Aasia');
+INSERT INTO alkupera VALUES (9, 'Kaakkois-Aasia');
+
 INSERT INTO myrkyllisyys VALUES (0, 'ei tiedossa');
-INSERT INTO aggressiivisuus VALUES (0, 'ei tiedossa');
+INSERT INTO myrkyllisyys VALUES (1, 'ei myrkyllinen');
+INSERT INTO myrkyllisyys VALUES (2, 'myrkyllinen');
+INSERT INTO myrkyllisyys VALUES (3, 'tappavan myrkyllinen');
 
  -- DROP TABLE kayttajat;
  -- DROP TABLE kaarmeet;
  -- DROP TABLE lajit;
  -- DROP TABLE lainat;
  -- DROP TABLE alkupera;
- -- DROP TABLE vari;
  -- DROP TABLE myrkyllisyys;
- -- DROP TABLE aggressiivisuus;
