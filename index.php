7317b69131d5f646990f916645c9e85188aae8d9
Tuomas Starck <dev@starck.fi>
2011-12-14 11:06:03 +0200

    Pääsivu ajan tasalle.

--- index.php
+++ index.php
@@ -1,15 +1,34 @@
 <?php
 
 require_once 'auth.php';
+require_once 'common.php';
+require_once 'kaarme.php';
 
-if (with(new AUTH)->ok()) {
-	header("Content-Type: application/xhtml+xml; charset=utf-8");
-	readfile("karmia.xhtml");
-}
-else {
-	header("Location: kirjaudu.xhtml");
+class INDEX {
+	/* Oletuskonstruktori:
+	 * 1) Tarkastetaan käyttäjä ja oikeudet,
+	 * 2) parsitaan syöte ja
+	 * 3) tehdään jotain järkevää edellisten perusteella.
+	 */
+	function __construct() {
+		$lupa = new AUTH;
+
+		if (!$lupa->ok()) {
+			header("Location: kirjaudu.xhtml");
+			exit;
+		}
+
+		with(new KAARME($lupa->kayttaja()))->juokse();
+
+		if (!headers_sent()) {
+			header("Content-Type: application/xhtml+xml; charset=utf-8");
+			readfile("json.xhtml");
+		}
+
+		exit;
+	}
 }
 
-exit;
+new INDEX;
 
 ?>
