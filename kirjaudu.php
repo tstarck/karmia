c1d782a4b0d82a3a3ad664e55911dfbe61aa8af7
Tuomas Starck <dev@starck.fi>
2011-12-20 10:15:13 +0200

    Refaktoroitu ja säädetty.

--- kirjaudu.php
+++ kirjaudu.php
@@ -4,23 +4,30 @@ require_once 'config/karmia.php';
 require_once 'auth.php';
 require_once 'common.php';
 
-$user = hae_arvo("user");
-$pass = hae_arvo("pass");
+class KIRJAUDU {
+	function __construct() {
+		global $_karmia_root;
 
-if (empty($user) or empty($pass)) {
-	if (with(new AUTH)->ok()) {
-		header("Location: ".$_karmia_root);
-	}
-	else {
-		header("Location: kirjaudu.xhtml#virheellinen_kirjautuminen");
-	}
+		if (with(new AUTH)->ok()) {
+			header("Location: ".$_karmia_root);
+			exit;
+		}
 
-	exit;
+		$user = hae_arvo("user");
+		$pass = hae_arvo("pass");
+
+		if (empty($user) or empty($pass)) {
+			header("Location: kirjaudu.xhtml#virheellinen_kirjautuminen");
+			exit;
+		}
+
+		aseta_pipari("user", $user);
+		aseta_pipari("pass", sha1($pass));
+
+		header("Location: ".$_SERVER["SCRIPT_NAME"]);
+	}
 }
 
-aseta_pipari("user", $user);
-aseta_pipari("pass", sha1($pass));
-
-header("Location: ".$_SERVER["SCRIPT_NAME"]);
+new KIRJAUDU;
 
 ?>
