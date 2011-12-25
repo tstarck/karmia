69540ee87ac7aa1d8c5cf62932875120d7c1734f
Tuomas Starck <dev@starck.fi>
2011-12-25 10:13:49 +0200

    Salaiset asetukset esille.

---
+++ config/karmia.php
@@ -0,0 +1,22 @@
+<?php
+
+/* Karmian asennuksen asiakkaalle näkyvä absoluuttinen polku.
+ *
+ * Esim: Karmian osoite                   |  $_karmia_root
+ *       ---------------------------------+------------------
+ *       http://karmia.info/              |  "/"
+ *       http://example.com/~user/karmia/ |  "/~user/karmia/"
+ */
+$_karmia_root = "/";
+
+/* Tietokantayhteyden tunnistemerkkijono.
+ *
+ * Tämä muuttuja annetaan parametrina PHP::pg_connect() -funktiolle ja sen
+ * tulee sisältää tarvittavat tiedot tietokantayhteyden avaamiseksi.
+ *
+ * Muuttujan muoto on määritelty kohdassa connection_string sivulla:
+ * http://fi.php.net/pg_connect
+ */
+$_karmia_db_config = "host=localhost user=kayttaja password=abcdef0123456789";
+
+?>
